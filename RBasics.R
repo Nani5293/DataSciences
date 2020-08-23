fdafc62ff8db8a0750d13f4b46a9f1888323fa9d
Nani5293 <45668179+Nani5293@users.noreply.github.com>
2020-08-23 18:15:38 +0530

    own

---
+++ RBasics.R
@@ -0,0 +1,33 @@
+ids = seq(1:10)
+ids
+ids[0]
+
+s = seq(1,10)
+m = matrix(s, nrow = 5 , ncol = 2)
+m
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ var.R
@@ -0,0 +1,41 @@
+pnorm(q = 63.68,mean = 0,sd = 1.04)
+v <-var(c(35,36,49,55,59,63,69,69,70,78,79,79,80,80))
+dev <-sqrt(v)
+dev
+
+
+dnorm(x = 63.68,mean = 0,sd = dev) 
+
+
+pnorm(q = 0.52,mean = 0 ,sd = 1,lower.tail = )
+qnorm(p = 0.7,mean = 0,sd = 1)
+
+
+set.seed(3)
+vec <-rnorm(10)
+
+
+vec
+m = mean(vec)
+sd =sqrt(var(vec))
+sd
+vec[2]
+
+
+pnorm(vec[1],mean = mean(vec),sd = sd)
+pnorm(vec[2],mean = mean(vec),sd = sd)
+pnorm(vec[3],mean = mean(vec),sd = sd)
+pnorm(vec[4],mean = mean(vec),sd = sd)
+pnorm(vec[5],mean = mean(vec),sd = sd)
+pnorm(vec[6],mean = mean(vec),sd = sd)
+pnorm(vec[7],mean = mean(vec),sd = sd)
+
+
+
+
+
+
+
+
+
+
